ec0ec29bb9d4e8945ee8e33659fd729b9e0cb96e
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-12 15:34:25 +0300

    v1.0.3

--- frappe_better_attach_control/public/js/better_attach_image.bundle.js
+++ frappe_better_attach_control/public/js/better_attach_image.bundle.js
@@ -1,8 +1,5 @@
-frappe.ui.form.ControlAttachImage = class ControlAttachImage extends frappe.ui.form.ControlAttachImage {
+frappe.ui.form.ControlAttachImage = class ControlAttachImage extends frappe.ui.form.ControlAttach {
     set_upload_options() {
         super.set_upload_options();
-        if (this.upload_options.restrictions.allowed_file_types == null) {
-            this.upload_options.restrictions.allowed_file_types = ['image/*'];
-        }
     }
 };
