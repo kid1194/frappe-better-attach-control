2a1d803370d68f3a6d269f9066fe5dabeacb8171
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 21:20:49 +0300

    Update to v1.1.4

--- frappe_better_attach_control/config/desktop.py
+++ frappe_better_attach_control/config/desktop.py
@@ -1,12 +1,12 @@
 from frappe import _
 
 def get_data():
-return [
-    {
-        "module_name": "Frappe Better Attach Control",
-        "color": "blue",
-        "icon": "octicon octicon-file-directory",
-        "type": "module",
-        "label": _("Frappe Better Attach Control")
-    }
-]
+    return [
+        {
+            "module_name": "Frappe Better Attach Control",
+            "color": "blue",
+            "icon": "octicon octicon-paperclip",
+            "type": "module",
+            "label": _("Frappe Better Attach Control")
+        }
+    ]
