fac6ab1ab37ba2075bcba1856f7834ceba5f861a
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 21:21:29 +0300

    Update to v1.1.4

--- frappe_better_attach_control/config/docs.py
+++ frappe_better_attach_control/config/docs.py
@@ -2,10 +2,5 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/frappe_better_attach_control"
-# docs_base_url = "https://[org_name].github.io/frappe_better_attach_control"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
-
 def get_context(context):
     context.brand_html = "Frappe Better Attach Control"
