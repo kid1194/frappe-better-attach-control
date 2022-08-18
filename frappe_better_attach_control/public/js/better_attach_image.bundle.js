43a632b2eaea2d398a9620d55436f178144a7f5e
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 21:28:30 +0300

    Update to v1.1.4

--- frappe_better_attach_control/public/js/better_attach_image.bundle.js
+++ frappe_better_attach_control/public/js/better_attach_image.bundle.js
@@ -1,19 +1,16 @@
-frappe.provide('frappe.ui.form');
-
 frappe.ui.form.ControlAttachImage = class ControlAttachImage extends frappe.ui.form.ControlAttach {
     _parse_options() {
         if (!this._is_better) this._images_only = true;
         super._parse_options();
     }
-    _make_value_dom() {
-        super._make_value_dom();
-        let val = this._value_to_array(this.value).pop();
-        let $file_link = this.$value.find('.attached-file-link');
-        $file_link.popover({
+    _on_setup_display(dom, url) {
+        var me = this;
+        if (!dom) dom = this.$value.find('.attached-file-link');
+        dom.popover({
             trigger: 'hover',
             placement: 'top',
             content: () => {
-                return '<div><img src="${val}" width="150px" style="object-fit: contain;"/></div>';
+                return '<div><img src="' + (url || me.value) + '" width="150px" style="object-fit: contain;"/></div>';
             },
             html: true
         });
