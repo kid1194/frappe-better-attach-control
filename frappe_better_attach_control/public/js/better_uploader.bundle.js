8263e8cafc298f66582e258633449816e61cf26b
kid1194 <kid1194@gmail.com>
2022-09-26 16:52:42 +0300

    Update to v1.1.7

--- frappe_better_attach_control/public/js/better_uploader.bundle.js
+++ frappe_better_attach_control/public/js/better_uploader.bundle.js
@@ -1,4 +1,142 @@
-import FileUploader from './file_uploader';
+import { get_image_type } from './utils/mime.js';
 
-frappe.provide('frappe.ui');
-frappe.ui.FileUploader = FileUploader;
+frappe.ui.FileUploader = class FileUploader extends frappe.ui.FileUploader {
+    constructor({
+        wrapper,
+        method,
+        on_success,
+        doctype,
+        docname,
+        fieldname,
+        files,
+        folder,
+        restrictions = {},
+        upload_notes,
+        allow_multiple,
+        as_dataurl,
+        disable_file_browser,
+        dialog_title,
+        attach_doc_image,
+        frm
+    } = {}) {
+        super({
+            wrapper,
+            method,
+            on_success,
+            doctype,
+            docname,
+            fieldname,
+            files,
+            folder,
+            restrictions = {},
+            upload_notes,
+            allow_multiple,
+            as_dataurl,
+            disable_file_browser,
+            dialog_title,
+            attach_doc_image,
+            frm
+        });
+        
+        this.uploader.add_files = function(file_array, ret) {
+            if (ret) {
+                file_array = Array.from(file_array)
+                    .map(file => {
+                        if (!file.name) {
+                            let path = file.file_url.toLowerCase().split('?')[0];
+                            file.name = file.file_name || path.split('/').pop();
+                            file.type = get_image_type(path.split('.').pop()) || '';
+                            file.size = 0;
+                        }
+                        return file;
+                    });
+            }
+            let files = Array.from(file_array);
+                .filter(this.check_restrictions)
+                .map(file => {
+                    let is_image =  file.type.startsWith('image');
+                    let size_kb = file.size / 1024;
+                    return {
+                        file_obj: file,
+                        cropper_file: file,
+                        crop_box_data: null,
+                        optimize: size_kb > 200 && is_image && !(file.type || '').includes('svg'),
+                        name: file.name,
+                        doc: null,
+                        progress: 0,
+                        total: 0,
+                        failed: false,
+                        request_succeeded: false,
+                        error_message: null,
+                        uploading: false,
+                        private: true
+                    };
+                });
+
+            // pop extra files as per FileUploader.restrictions.max_number_of_files
+            max_number_of_files = this.restrictions.max_number_of_files;
+            if (max_number_of_files && files.length > max_number_of_files) {
+                files.slice(max_number_of_files).forEach(file => {
+                    this.show_max_files_number_warning(file, this.doctype);
+                });
+
+                files = files.slice(0, max_number_of_files);
+            }
+            
+            if (ret) return files;
+
+            this.files = this.files.concat(files);
+            // if only one file is allowed and crop_image_aspect_ratio is set, open cropper immediately
+            if (this.files.length === 1 && !this.allow_multiple && this.restrictions.crop_image_aspect_ratio != null) {
+                if (!this.files[0].file_obj.type.includes('svg')) {
+                    this.toggle_image_cropper(0);
+                }
+            }
+        };
+        this.uploader.upload_via_file_browser = function() {
+            let selected_file = this.$refs.file_browser.selected_node;
+            if (!selected_file.value) {
+                frappe.msgprint(__('Click on a file to select it.'));
+                this.close_dialog = true;
+                return Promise.reject();
+            }
+            this.close_dialog = true;
+            return this.upload_file({
+                file_url: selected_file.file_url
+            }, true);
+        };
+        this.uploader.upload_via_web_link = function() {
+            let file_url = this.$refs.web_link.url;
+            if (!file_url) {
+                frappe.msgprint(__('Invalid URL'));
+                this.close_dialog = true;
+                return Promise.reject();
+            }
+            file_url = decodeURI(file_url)
+            this.close_dialog = true;
+            return this.upload_file({
+                file_url
+            }, true);
+        };
+        var old_upload_file = this.uploader.upload_file;
+        this.uploader.upload_file = function(file, i) {
+            if (i === true) {
+                i = undefined;
+                file = this.add_files([file], true);
+                if (!file.length) return;
+                file = file[0];
+            }
+            old_upload_file.call(this, file);
+        };
+        this.uploader.google_drive_callback = function(data) {
+            if (data.action == google.picker.Action.PICKED) {
+                this.upload_file({
+                    file_url: data.docs[0].url,
+                    file_name: data.docs[0].name
+                }, true);
+            } else if (data.action == google.picker.Action.CANCEL) {
+                cur_frm.attachments.new_attachment();
+            }
+        };
+    }
+};
--- frappe_better_attach_control/public/js/better_uploader_v13.bundle.js
+++ frappe_better_attach_control/public/js/better_uploader_v13.bundle.js
@@ -1,4 +1,109 @@
-import FileUploader from './file_uploader/v13';
+import { get_image_type } from './utils/mime.js';
 
-frappe.provide('frappe.ui');
-frappe.ui.FileUploader = FileUploader;
+frappe.ui.FileUploader = class FileUploader extends frappe.ui.FileUploader {
+    constructor({
+        wrapper,
+        method,
+        on_success,
+        doctype,
+        docname,
+        fieldname,
+        files,
+        folder,
+        restrictions,
+        upload_notes,
+        allow_multiple,
+        as_dataurl,
+        disable_file_browser,
+        frm
+    } = {}) {
+        super({
+            wrapper,
+            method,
+            on_success,
+            doctype,
+            docname,
+            fieldname,
+            files,
+            folder,
+            restrictions,
+            upload_notes,
+            allow_multiple,
+            as_dataurl,
+            disable_file_browser,
+            frm
+        });
+        
+        this.uploader.add_files = function(file_array, ret) {
+            if (ret) {
+                file_array = Array.from(file_array)
+                    .map(file => {
+                        if (!file.name) {
+                            let path = file.file_url.toLowerCase().split('?')[0];
+                            file.name = file.file_name || path.split('/').pop();
+                            file.type = get_image_type(path.split('.').pop()) || '';
+                        }
+                        return file;
+                    });
+            }
+            let files = Array.from(file_array)
+                .filter(this.check_restrictions)
+                .map(file => {
+                    let is_image = file.type.startsWith('image');
+                    return {
+                        file_obj: file,
+                        name: file.name,
+                        doc: null,
+                        progress: 0,
+                        total: 0,
+                        failed: false,
+                        uploading: false,
+                        private: !is_image
+                    }
+                });
+            if (ret) return files;
+            this.files = this.files.concat(files);
+        };
+        this.uploader.upload_via_file_browser = function() {
+            let selected_file = this.$refs.file_browser.selected_node;
+            if (!selected_file.value) {
+                frappe.msgprint(__('Click on a file to select it.'));
+                return Promise.reject();
+            }
+            return this.upload_file({
+                file_url: selected_file.file_url
+            }, true);
+        };
+        this.uploader.upload_via_web_link = function() {
+            let file_url = this.$refs.web_link.url;
+            if (!file_url) {
+                frappe.msgprint(__('Invalid URL'));
+                return Promise.reject();
+            }
+            file_url = decodeURI(file_url)
+            return this.upload_file({
+                file_url
+            }, true);
+        };
+        var old_upload_file = this.uploader.upload_file;
+        this.uploader.upload_file = function(file, i) {
+            if (i === true) {
+                i = undefined;
+                file = this.add_files([file], true);
+                if (!file.length) return;
+                file = file[0];
+            }
+            old_upload_file.call(this, file);
+        };
+        this.uploader.google_drive_callback = function(data) {
+            if (data.action == google.picker.Action.PICKED) {
+                this.upload_file({
+                    file_url: data.docs[0].url,
+                    file_name: data.docs[0].name
+                }, true);
+            } else if (data.action == google.picker.Action.CANCEL) {
+                dialog.show();
+            }
+        };
+    }
+};
