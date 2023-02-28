72560fb6c1638500779a9b8a1951525ec0ff7776
kid1194 <kid1194@gmail.com>
2023-03-01 02:12:42 +0300

    v2-Beta11 release

--- frappe_better_attach_control/api/website.py
+++ frappe_better_attach_control/api/website.py
@@ -10,38 +10,56 @@ from .common import error
 
 
 def website_context(context):
-    if context.get("web_form_doc"):
-        app_name = "frappe_better_attach_control"
+    if context.get("doc") and context.doc.doctype == "Web Form" and context.doc.name:
+        fields = 0
         
         try:
-            js_files = frappe.get_hooks("better_webform_include_js", default=None, app_name=app_name)
-            if js_files:
-                if not isinstance(js_files, list):
-                    js_files = [js_files]
-                
-                script = context.get("script", "")
-                for path in js_files:
-                    path = frappe.get_app_path(app_name, *path.strip("/").split("/"))
-                    custom_js = frappe.render_template(open(path).read(), context)
-                    script = "\n\n".join([script, custom_js])
-                    
-                context.script = script
+            fields = frappe.db.count(
+                "Web Form Field",
+                {
+                    "parent": context.doc.name,
+                    "parenttype": "Web Form",
+                    "parentfield": "web_form_fields",
+                    "fieldtype": ["in", ["Attach", "Attach Image"]],
+                }
+            )
         except Exception:
-            error("Unable to inject the js files to context.", throw=False)
+            fields = 0
+            error("Unable to get the Attach fields of the web form.", throw=False)
         
-        try:
-            css_files = frappe.get_hooks("better_webform_include_css", default=None, app_name=app_name)
-            if css_files:
-                if not isinstance(css_files, list):
-                    css_files = [css_files]
-                
-                style = context.get("style", "")
-                for path in css_files:
-                    path = frappe.get_app_path(app_name, *path.strip("/").split("/"))
-                    custom_css = open(path).read()
-                    style = "\n\n".join([style, custom_css])
+        if fields > 0:
+        
+            app_name = "frappe_better_attach_control"
+            
+            try:
+                js_files = frappe.get_hooks("better_webform_include_js", default=None, app_name=app_name)
+                if js_files:
+                    if not isinstance(js_files, list):
+                        js_files = [js_files]
                     
-                context.style = style
-        
-        except Exception:
+                    script = context.get("script", "")
+                    for path in js_files:
+                        path = frappe.get_app_path(app_name, *path.strip("/").split("/"))
+                        custom_js = frappe.render_template(open(path).read(), context)
+                        script = "\n\n".join([script, custom_js])
+                        
+                    context.script = script
+            except Exception:
+                error("Unable to inject the js files to context.", throw=False)
+            
+            try:
+                css_files = frappe.get_hooks("better_webform_include_css", default=None, app_name=app_name)
+                if css_files:
+                    if not isinstance(css_files, list):
+                        css_files = [css_files]
+                    
+                    style = context.get("style", "")
+                    for path in css_files:
+                        path = frappe.get_app_path(app_name, *path.strip("/").split("/"))
+                        custom_css = open(path).read()
+                        style = "\n\n".join([style, custom_css])
+                        
+                    context.style = style
+            
+            except Exception:
             error("Unable to inject the css files to context.", throw=False)
